47cea4824f7b3a803cab76b3af4c6f92ac53715d
csaim <gavderavi4@gmail.com>
2024-08-11 17:23:25 -0400

    added 1 thing, idk what

--- app/src/main/java/com/example/firebaseauthapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthapp/MainActivity.kt
@@ -6,13 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebaseauthapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.signOutBtn.setOnClickListener{
+            Firebase.auth.signOut()
+        }
 
     }
 }
